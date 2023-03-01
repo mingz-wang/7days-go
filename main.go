439198f586e799b9ac2c45c8ca9b80a22b5edf57
mingz-wang <mingz.wang@foxmail.com>
2023-03-01 18:59:04 +0800

    使用New()创建 gee 的实例，使用 GET()方法添加路由，最后使用Run()启动Web服务。

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"gee"
+)
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+	// TODO: 这里的路由，只是静态路由，不支持/hello/:name这样的动态路由，动态路由我们将在下一次实现。
+	r.Run(":8080")
 }
