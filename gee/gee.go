2e382182f99ac6e69b2c7456d8440618d85f6672
mingz-wang <mingz.wang@foxmail.com>
2023-03-01 18:52:08 +0800

    首先定义了类型HandlerFunc，这是提供给框架用户的，用来定义路由映射的处理方法。我们在Engine中，添加了一张路由映射表router，key 由请求方法和静态路由地址构成，例如GET-/、GET-/hello、POST-/hello，这样针对相同的路由，如果请求方法不同,可以映射不同的处理方法(Handler)，value 是用户映射的处理方法。
    当用户调用(*Engine).GET()方法时，会将路由和处理方法注册到映射表 router 中，(*Engine).Run()方法，是 ListenAndServe 的包装。
    
    Engine实现的 ServeHTTP 方法的作用就是，解析请求的路径，查找路由映射表，如果查到，就执行注册的处理方法。如果查不到，就返回 404 NOT FOUND 。

--- gee/gee.go
+++ gee/gee.go
@@ -1,1 +1,48 @@
 package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+// Engine implement the interface of ServeHttp
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New is the Constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (e *Engine) addRoute(method, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	e.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (e *Engine) GET(pattern string, handler HandlerFunc) {
+	e.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (e *Engine) POST(pattern string, handler HandlerFunc) {
+	e.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (e *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, e)
+}
+
+func (e *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := e.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
