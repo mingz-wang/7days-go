87fd7a825c0a74cec418e9c8fc49fab615933a58
mingz-wang <mingz.wang@foxmail.com>
2023-03-04 21:29:59 +0800

    分组控制

--- gee/gee.go
+++ gee/gee.go
@@ -1,34 +1,63 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 // HandlerFunc defines the request handler used by gee
 type HandlerFunc func(c *Context)
 
+// RouterGroup 分组控制
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share an Engine instance
+}
+
 // Engine implement the interface of ServeHttp
 type Engine struct {
-	router *router
+	*RouterGroup // 匿名字段(Anonymous Fields)
+	router       *router
+	groups       []*RouterGroup // store all groups
 }
 
 // New is the Constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (e *Engine) addRoute(method, pattern string, handler HandlerFunc) {
-	e.router.addRoute(method, pattern, handler)
+// Group is define to create a new RouterGroup
+// remember all group share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %5s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
 // POST defines the method to add POST request
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 // Run defines the method to start a http server
